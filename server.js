b2e4376f816b0f7fce18445ed1308bdccbbcdd7e
uiido <uiido@yahoo.com>
2023-01-10 02:02:08 -0500

    Filled in more of server.js - node does not currently run

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const inquirer = require('inquirer');
 const prompt = inquirer.createPromptModule();
 const mysql = require('mysql2');
+require('console.table');
 
 const db = mysql.createConnection({
     user: "root",
     database: "employee_db",
 });
+
+switch (type) {
+    case 'VIEW ALL EMPLOYEES': {
+        db.query('SELECT * FROM employee', (err, employees) => {
+            console.table(employees);
+        });
+    }
+    case 'VIEW ALL DEPARTMENTS': {
+        db.query('SELECT * FROM department', (err, departments) => {
+            console.table(departments);
+        });
+    }
+    case 'VIEW ALL ROLES': {
+        db.query('SELECT * FROM role', (err, roles) => {
+            console.table(roles);
+        });
+    }
+}
