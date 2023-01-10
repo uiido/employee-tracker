5db444366392b4eca423502b340041b27af49e74
uiido <uiido@yahoo.com>
2023-01-10 00:57:08 -0500

    Began filling in schema

--- schema.sql
+++ schema.sql
@@ -1,38 +1,18 @@
-DROP DATABASE IF EXISTS registrar_db;
-CREATE DATABASE registrar_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE registrar_db;
+USE employee_db;
 
-CREATE TABLE instructors (
-  id INT NOT NULL,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  PRIMARY KEY (id)
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) not null,
 );
 
-CREATE TABLE courses (
-  id INT,
-  course_title VARCHAR(30) NOT NULL,
-  instructor_id INT,
-  order_details TEXT,
-  FOREIGN KEY (instructor_id)
-  REFERENCES instructors(id)
-  ON DELETE SET NULL
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT
 );
 
-INSERT INTO instructors (id, first_name, last_name)
-  VALUES (1, 'Omari', 'Hardwick');
-INSERT INTO courses (id, course_title, instructor_id, order_details)
-  VALUES (1, 'Streets Smarts 101', 1, 'Pre Req, learning how to hustle');
-
-SELECT
-  courses.id,
-  course_title,
-  CONCAT(
-    first_name,
-    ' ',
-    last_name
-  ) AS instructor
-FROM courses
-JOIN instructors
-ON courses.instructor_id = instructors.id;
+CREATE TABLE employee ();
