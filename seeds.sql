7623d0c78e3aed21eca2fcbbd5245687c5f53164
uiido <uiido@yahoo.com>
2023-01-10 01:40:04 -0500

    Filled in further seed information

--- seeds.sql
+++ seeds.sql
@@ -7,9 +7,9 @@ INSERT INTO department (name)
 
 INSERT INTO role (title, salary, department_id)
     VALUES
-    ('Concept Artist', 50000, 1),
     ('Lead Animator', 60000, 1),
     ('Animator', 40000, 1),
+    ('Concept Artist', 50000, 1),
     ('Background Painter', 40000, 1),
     ('Composer', 45000, 2),
     ('Foley Artist', 40000, 2),
@@ -21,14 +21,14 @@ INSERT INTO role (title, salary, department_id)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES
-    ('Tyrus', 'Wong', 1),
-    ('William', 'Hanna', 2),
-    ('Joseph', 'Barbera', 3),
-    ('Toby', 'Bluth', 4),
-    ('Joe', 'Hisaishi', 5),
-    ('Jimmy MacDonald', 6),
-    ('Idina', 'Menzel', 7),
-    ('Brad', 'Bird', 8),
-    ('Wes', 'Anderson', 9),
-    ('Jim', 'Cummings', 10),
-    ('Mel', 'Blanc', 11)
+    ('William', 'Hanna', 2, null),
+    ('Joseph', 'Barbera', 3, 1),
+    ('Tyrus', 'Wong', 1, 1),
+    ('Toby', 'Bluth', 4, 1),
+    ('Joe', 'Hisaishi', 5, null),
+    ('Jimmy MacDonald', 6, 5),
+    ('Idina', 'Menzel', 7, 5),
+    ('Brad', 'Bird', 8, null),
+    ('Wes', 'Anderson', 9, 8),
+    ('Jim', 'Cummings', 10, null),
+    ('Mel', 'Blanc', 11, 10)
