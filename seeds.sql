428b01a4f523ebecb261a9858a93f1a893dc33df
uiido <uiido@yahoo.com>
2023-01-10 01:31:35 -0500

    Filled in more seed information

--- seeds.sql
+++ seeds.sql
@@ -1,6 +1,34 @@
-INSERT INTO DEPARTMENT (name)
+INSERT INTO department (name)
     VALUES 
     ('Art'),
     ('Sound'),
     ('Writing'),
     ('Acting')
+
+INSERT INTO role (title, salary, department_id)
+    VALUES
+    ('Concept Artist', 1),
+    ('Lead Animator', 1),
+    ('Animator', 1),
+    ('Background Painter', 1),
+    ('Composer', 2),
+    ('Foley Artist', 2),
+    ('Singer', 2),
+    ('Screenwriter', 3),
+    ('Storyboarder', 3),
+    ('Actor', 4),
+    ('Extra Voices', 4)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES
+    ('Tyrus', 'Wong'),
+    ('William', 'Hanna'),
+    ('Joseph', 'Barbera'),
+    ('Toby', 'Bluth'),
+    ('Joe', 'Hisaishi'),
+    ('Jimmy MacDonald', ''),
+    ('Idina', 'Menzel'),
+    ('Brad', 'Bird'),
+    ('Wes', 'Anderson'),
+    ('Jim', 'Cummings'),
+    ('Mel', 'Blanc')
