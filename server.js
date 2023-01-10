532834326bde8c5d0adf9eca6d276cb5c8fec987
uiido <uiido@yahoo.com>
2023-01-10 02:12:55 -0500

    Began adding prompts to server.js - node not properly working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const prompt = inquirer.createPromptModule();
-const mysql = require('mysql2');
+const mysql = require('mysql');
 require('console.table');
 
 const db = mysql.createConnection({
@@ -8,20 +8,26 @@ const db = mysql.createConnection({
     database: "employee_db",
 });
 
-switch (type) {
-    case 'VIEW ALL EMPLOYEES': {
-        db.query('SELECT * FROM employee', (err, employees) => {
-            console.table(employees);
-        });
+const chooseOption = (type) => {
+    switch (type) {
+        case 'VIEW ALL EMPLOYEES': {
+            db.query('SELECT * FROM employee', (err, employees) => {
+                console.table(employees);
+            });
+        }
+        case 'VIEW ALL DEPARTMENTS': {
+            db.query('SELECT * FROM department', (err, departments) => {
+                console.table(departments);
+            });
+        }
+        case 'VIEW ALL ROLES': {
+            db.query('SELECT * FROM role', (err, roles) => {
+                console.table(roles);
+            });
+        }
     }
-    case 'VIEW ALL DEPARTMENTS': {
-        db.query('SELECT * FROM department', (err, departments) => {
-            console.table(departments);
-        });
-    }
-    case 'VIEW ALL ROLES': {
-        db.query('SELECT * FROM role', (err, roles) => {
-            console.table(roles);
-        });
-    }
-}
+};
+
+prompt({
+
+});
