895d36b4c1a4ebc011a6d268a7969820c6770954
uiido <uiido@yahoo.com>
2023-01-11 00:17:29 -0500

    Updated schema to get employees to run properly

--- schema.sql
+++ schema.sql
@@ -6,26 +6,28 @@ USE employee_db;
 
 CREATE TABLE
     department (
-        id INT NOT NULL AUTO_INCREMENT,
+        department_id INT NOT NULL AUTO_INCREMENT,
         name VARCHAR(30) NOT NULL,
         PRIMARY KEY (id)
     );
 
 CREATE TABLE
     role (
-        id INT NOT NULL AUTO_INCREMENT,
+        role_id INT NOT NULL AUTO_INCREMENT,
         title VARCHAR(30) NOT NULL,
         salary DECIMAL NOT NULL,
         department_id INT NOT NULL,
-        PRIMARY KEY (id)
+        PRIMARY KEY (id),
+        FOREIGN KEY (department_id) REFERENCES departments(departments_id)
     );
 
 CREATE TABLE
     employee (
-        id INT NOT NULL AUTO_INCREMENT,
+        employee_id INT NOT NULL AUTO_INCREMENT,
         first_name VARCHAR(30) NOT NULL,
         last_name VARCHAR(30) NOT NULL,
-        role_id INT NOT NULL,
+        role_id INT,
         manager_id INT,
-        PRIMARY KEY (id)
+        PRIMARY KEY (id),
+        FOREIGN KEY (manager_id) REFERENCES employee (employee_id)
     );
--- seeds.sql
+++ seeds.sql
@@ -1,36 +1,17 @@
 USE employee_db;
 
 INSERT INTO department (name)
-    VALUES 
-    ('Art'),
-    ('Sound'),
-    ('Writing'),
-    ('Acting');
+VALUES ('Art'), ('Sound'), ('Writing'), ('Acting');
 
-INSERT INTO role (title, salary, department_id)
-    VALUES
-    ('Lead Animator', 60000, 1),
-    ('Animator', 40000, 1),
-    ('Concept Artist', 50000, 1),
-    ('Background Painter', 40000, 1),
-    ('Composer', 45000, 2),
-    ('Foley Artist', 40000, 2),
-    ('Singer', 60000, 2),
-    ('Screenwriter', 60000, 3),
-    ('Storyboarder', 60000, 3),
-    ('Actor', 65000, 4),
-    ('Extra Voices', 35000, 4);
+INSERT INTO
+    role (title, salary, department_id)
+VALUES ('Lead Animator', 60000, 1), ('Animator', 40000, 1), ('Concept Artist', 50000, 1), ('Background Painter', 40000, 1), ('Composer', 45000, 2), ('Foley Artist', 40000, 2), ('Singer', 60000, 2), ('Screenwriter', 60000, 3), ('Storyboarder', 60000, 3), ('Actor', 65000, 4), ('Extra Voices', 35000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VALUES
-    ('William', 'Hanna', 2, null),
-    ('Joseph', 'Barbera', 3, 1),
-    ('Tyrus', 'Wong', 1, 1),
-    ('Toby', 'Bluth', 4, 1),
-    ('Joe', 'Hisaishi', 5, null),
-    ('Jimmy', 'MacDonald', 6, 5),
-    ('Idina', 'Menzel', 7, 5),
-    ('Brad', 'Bird', 8, null),
-    ('Wes', 'Anderson', 9, 8),
-    ('Jim', 'Cummings', 10, null),
-    ('Mel', 'Blanc', 11, 10);
+INSERT INTO
+    employee (
+        first_name,
+        last_name,
+        role_id,
+        manager_id
+    )
+VALUES ('William', 'Hanna', 2, null), ('Joseph', 'Barbera', 3, 1), ('Tyrus', 'Wong', 1, 1), ('Toby', 'Bluth', 4, 1), ('Joe', 'Hisaishi', 5, null), ('Jimmy', 'MacDonald', 6, 5), ('Idina', 'Menzel', 7, 5), ('Brad', 'Bird', 8, null), ('Wes', 'Anderson', 9, 8), ('Jim', 'Cummings', 10, null), ('Mel', 'Blanc', 11, 10);
