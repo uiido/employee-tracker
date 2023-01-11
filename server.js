321e84b807b526dfeae70ee10a5cc9edf52b7a37
uiido <uiido@yahoo.com>
2023-01-10 23:26:20 -0500

    Removed instructor provided reference code. Added employee info function. Small changes.

--- server.js
+++ server.js
@@ -10,13 +10,53 @@ const connection = mysql.createConnection({
     database: "employee_db",
 });
 
+// Delivers results for all prompts
+const selectAll = async (table, display) => {
+    const results = await db.promise().query('SELECT * FROM ' + table);
+    if (display) {
+        console.table(results[0]);
+        return init();
+    }
+    return results;
+};
+
+// Inserts data for new employee
+const insert = (table, data) => {
+    db.query('INSERT INTO ?? SET ?', [table, data], (err) => {
+        if (err) return console.error(err);
+        console.log('\nYour new employee has been added!\n');
+        init();
+    });
+};
+
 // Returns all employees' values
 const selectAllNameAndValue = (table, name, value) => {
     return db.promise().query('SELECT ?? AS name, ?? AS value FROM ??', [name, value, table]);
 };
 
 // Returns all employees' information
-
+const selectAllEmployeeDetails = async () => {
+    const statement = `
+SELECT
+  employee.id,
+  employee.first_name,
+  employee.last_name,
+  role.title,
+  role.salary,
+  CONCAT(
+    manager.first_name,
+    ' ',
+    manager.last_name
+  ) AS manager
+FROM employee
+JOIN role
+ON employee.role_id = role.id
+JOIN employee AS manager
+ON employee.manager_id = manager.id
+  `
+    const [employees] = await db.promise().query(statement);
+    console.table(employees);
+};
 
 // Function that creates new employees 
 const addNewEmployee = async () => {
@@ -112,88 +152,3 @@ const init = () => {
 
 // Runs init function
 init();
-
-
-
-
-
-
-
-
-
-
-
-const selectAll = async (table, display) => {
-    const results = await db.promise().query('SELECT * FROM ' + table);
-    if (display) {
-        console.table(results[0]);
-        return init();
-    }
-    return results;
-};
-
-const insert = (table, data) => {
-    db.query('INSERT INTO ?? SET ?', [table, data], (err) => {
-        if (err) return console.error(err);
-        console.log('\nSuccesfully created employee!\n');
-        init();
-    });
-};
-
-// const selectAllNameAndValue = (table, name, value) => {
-//     return db.promise().query('SELECT ?? AS name, ?? AS value FROM ??', [name, value, table]);
-// };
-
-const selectAllEmployeeDetails = async () => {
-    const statement = `
-SELECT
-  employee.id,
-  employee.first_name,
-  employee.last_name,
-  role.title,
-  role.salary,
-  CONCAT(
-    manager.first_name,
-    ' ',
-    manager.last_name
-  ) AS manager
-FROM employee
-JOIN role
-ON employee.role_id = role.id
-JOIN employee AS manager
-ON employee.manager_id = manager.id
-  `
-    const [employees] = await db.promise().query(statement);
-    console.table(employees);
-};
-
-// const addEmployee = async () => {
-//     const [roles] = await selectAllNameAndValue('role', 'title', 'id');
-//     const [managers] = await selectAllNameAndValue('employee', 'last_name', 'id');
-//     prompt([
-//         {
-//             name: 'first_name',
-//             message: 'Enter the employee\'s first name.',
-//         },
-//         {
-//             name: 'last_name',
-//             message: 'Enter the employee\'s last name.',
-//         },
-//         {
-//             type: 'rawlist',
-//             name: 'role_id',
-//             message: 'Choose a role for this employee.',
-//             choices: roles,
-//         },
-//         {
-//             type: 'rawlist',
-//             name: 'manager_id',
-//             message: 'Choose a manager for this employee.',
-//             choices: managers,
-//         }
-//     ])
-//         .then((answers) => {
-//             insert('employee', answers);
-//         });
-// };
-
